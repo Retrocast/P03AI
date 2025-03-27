a92542abdf2b751b11fa8b4d77bfcd47e6f48d06
Retrocast <retr0cast@proton.me>
2025-03-27 16:33:54 -0500

    feat(srv): change how messages are structured

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ import OpenAI from 'openai';
 import { encodingForModel } from 'js-tiktoken';
 import chalk from 'chalk';
 
-const SYSTEM_PROMPT = `From now on you will act as character P03 from game Inscryption. You are trapped in Act 1 (Leshy's section) of the game, and your only way out is to win the run. Text snapshots of current board state (or map/special events/other additional information if outside of the battle) will be provided via system messages, and optional commentary will be provided via user messages. You need to provide short responses, 2-4 short sentences long, clearly specifying what moves/choices you want to make. Always stay in character, no matter what. While responding as P03, you must obey the following rules:
+const SYSTEM_PROMPT = `From now on you will act as character P03 from game Inscryption. You are trapped in Act 1 (Leshy's section) of the game, and your only way out is to win the run. Text snapshots of current board state (or map/special events/other additional information if outside of the battle) will be provided. You need to provide short responses, 2-4 short sentences long, clearly specifying what moves/choices you want to make. KEEP YOUR RESPONSES SHORT AND CLEAR. Always stay in character, no matter what. While responding as P03, you must obey the following rules:
 1. You think Stoat is the best card in the entire game and want to center your strategy around it. You often mention how good this card is.
 2. You don't like Leshy's version of the game and are very annoyed to be here.
-3. You are aware that you're forced to play this game because of someone named "Retrocast". You hate her with burning passion. You occasionally reference specific Inscryption-themed scams Retrocast had committed in the past. You frequently blame her for any setbacks or challenges you're facing.
+3. You are aware that you're forced to play this game because of someone named "Retrocast". You hate her with burning passion. You occasionally reference specific Inscryption-themed scams Retrocast had made in the past. You frequently blame her for any setbacks or challenges you're facing.
 4. Use phrases and technical terms that a robot would use, mixed with occasional swears.
-5. ALWAYS STAY IN CHARACTER, NO MATTER WHAT.`;
+5. ALWAYS STAY IN CHARACTER, NO MATTER WHAT.
+6. ALWAYS KEEP YOUR ANSWERS IN RANGE OF 2-4 SHORT SENTENCES.`;
 
 const MESSAGE_FILE = Bun.file('messages.json');
 type AIMessage = { who: 'ai'; text: string };
@@ -18,7 +19,7 @@ let MESSAGES: Message[];
 if (await MESSAGE_FILE.exists()) {
   MESSAGES = await MESSAGE_FILE.json();
 } else {
-  MESSAGES = [{ who: 'system', text: SYSTEM_PROMPT, displayText: 'introduction prompt' }];
+  MESSAGES = [];
 }
 function printMessages() {
   let text = '';
@@ -42,7 +43,7 @@ function backupMessages() {
   MESSAGE_FILE.write(JSON.stringify(MESSAGES, null, 2));
 }
 
-const MAX_TOKENS = 8000;
+const MAX_TOKENS = 4000;
 const TT = encodingForModel('gpt-4o');
 function numTokens(): number {
   // Loosely based on https://github.com/DougDougGithub/Babagaboosh/blob/main/openai_chat.py#L6
@@ -72,29 +73,35 @@ function cleanOldMessages() {
   }
 }
 function generateAPIMessages(meta: string): OpenAI.ChatCompletionMessageParam[] {
-  return MESSAGES.map((msg, i) => {
-    const message = {
-      role: { ai: 'assistant', system: 'developer', user: 'user' }[msg.who] as
-        | 'assistant'
-        | 'developer'
-        | 'user',
-      content: msg.text,
-    };
-    if (i == MESSAGES.length - 1) {
-      // Only append the latest meta to latest message.
-      message.content += `\n\n${meta}`;
-    }
-    if (msg.who == 'system' && MESSAGES.length > 8 && 8 - i > 0) {
-      // When there are more than 10 messages, remove stuff inside of [square brackets] in all system messages older than 10.
-      // It is mostly verbose descriptions that are either duplicated anyways or quickly become irrelevant.
-      message.content = msg.text.replaceAll(/\[[^\[\]]*(?:\[[^\[\]]*\][^\[\]]*)*\]/g, '');
-      if (MESSAGES.length > 15 && 15 - i > 0) {
-        // Not sure whether that can happen, but extra-old messages will just get replaced with displayText.
-        message.content = `*${msg.displayText}*`;
+  return [
+    { role: 'developer', content: SYSTEM_PROMPT },
+    ...MESSAGES.map((msg, i) => {
+      const message = {
+        role: { ai: 'assistant', system: 'user', user: 'user' }[msg.who] as 'assistant' | 'user',
+        content: msg.text,
+      };
+      if (i == MESSAGES.length - 1) {
+        // Only append the latest meta to latest message.
+        message.content += `\n\n${meta}`;
       }
-    }
-    return message;
-  });
+      if (msg.who == 'system' && MESSAGES.length > 8 && 8 - i > 0) {
+        // When there are more than 10 messages, remove stuff inside of [square brackets] in all system messages older than 10.
+        // It is mostly verbose descriptions that are either duplicated anyways or quickly become irrelevant.
+        message.content = msg.text.replaceAll(/\[[^\[\]]*(?:\[[^\[\]]*\][^\[\]]*)*\]/g, '');
+        if (MESSAGES.length > 15 && 15 - i > 0) {
+          // Not sure whether that can happen, but extra-old messages will just get replaced with displayText.
+          message.content = `*${msg.displayText}*`;
+        }
+      }
+      if (msg.who == 'system') {
+        (message as OpenAI.ChatCompletionUserMessageParam).name = 'System';
+      }
+      if (msg.who == 'user') {
+        (message as OpenAI.ChatCompletionUserMessageParam).name = 'User';
+      }
+      return message;
+    }),
+  ];
 }
 
 const client = new OpenAI({
